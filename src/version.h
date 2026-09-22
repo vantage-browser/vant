0b9b43159621a23be926ea113f951baabd4ab03e
n-ham <nicholas.charles.ham@gmail.com>
2026-09-22 11:01:54 +0000

    Bump version to 0.1.7

--- src/version.h
+++ src/version.h
@@ -4,7 +4,7 @@
 #include <string_view>
 
 namespace vantage {
-inline constexpr std::string_view version = "0.1.6";
+inline constexpr std::string_view version = "0.1.7";
 }
 
 #endif
--- tests/version.cpp
+++ tests/version.cpp
@@ -4,6 +4,6 @@
 #include <string_view>
 
 int main() {
-    static_assert(vantage::version == std::string_view{"0.1.6"});
+    static_assert(vantage::version == std::string_view{"0.1.7"});
     assert(!vantage::version.empty());
 }
